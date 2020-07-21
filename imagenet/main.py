90f09c3289bd2d26ab4e72972ab5aa369059b3c0
Zhang, Xiaobing <xiaobing.zhang@intel.com>
2020-07-21 10:52:54 +0800

    enable mkldnn backend inference

--- imagenet/main.py
+++ imagenet/main.py
@@ -17,6 +17,7 @@ import torch.utils.data.distributed
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
+from torch.utils import mkldnn as mkldnn_utils
 
 model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
@@ -73,24 +74,39 @@ parser.add_argument('--multiprocessing-distributed', action='store_true',
                          'N processes per node, which has N GPUs. This is the '
                          'fastest way to use PyTorch for either single node or '
                          'multi node data parallel training')
+parser.add_argument('--mkldnn', action='store_true', default=False,
+                            help='use mkldnn weight cache')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                            help='disable CUDA')
+parser.add_argument("--dummy", action='store_true',
+                    help="using  dummu data to test the performance of inference")
 
 best_acc1 = 0
 
 
 def main():
     args = parser.parse_args()
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+    print(args)
+    # disable mkldnn
+    if not args.mkldnn:
+        torch._C._set_mkldnn_enabled(False)
+    if args.mkldnn and not args.cuda:
+        torch._C._set_mkldnn_enabled(True)
 
     if args.seed is not None:
         random.seed(args.seed)
         torch.manual_seed(args.seed)
-        cudnn.deterministic = True
+        if args.cuda:
+            cudnn.deterministic = True
         warnings.warn('You have chosen to seed training. '
                       'This will turn on the CUDNN deterministic setting, '
                       'which can slow down your training considerably! '
                       'You may see unexpected behavior when restarting '
                       'from checkpoints.')
 
-    if args.gpu is not None:
+    if args.gpu is not None and args.cuda:
         warnings.warn('You have chosen a specific GPU. This will completely '
                       'disable data parallelism.')
 
@@ -99,7 +115,7 @@ def main():
 
     args.distributed = args.world_size > 1 or args.multiprocessing_distributed
 
-    ngpus_per_node = torch.cuda.device_count()
+    ngpus_per_node = torch.cuda.device_count() if args.cuda else 0
     if args.multiprocessing_distributed:
         # Since we have ngpus_per_node processes per node, the total world_size
         # needs to be adjusted accordingly
@@ -116,7 +132,7 @@ def main_worker(gpu, ngpus_per_node, args):
     global best_acc1
     args.gpu = gpu
 
-    if args.gpu is not None:
+    if args.gpu is not None and args.cuda:
         print("Use GPU: {} for training".format(args.gpu))
 
     if args.distributed:
@@ -156,19 +172,24 @@ def main_worker(gpu, ngpus_per_node, args):
             # DistributedDataParallel will divide and allocate batch_size to all
             # available GPUs if device_ids are not set
             model = torch.nn.parallel.DistributedDataParallel(model)
-    elif args.gpu is not None:
+    elif args.gpu is not None and args.cuda:
         torch.cuda.set_device(args.gpu)
         model = model.cuda(args.gpu)
     else:
         # DataParallel will divide and allocate batch_size to all available GPUs
         if args.arch.startswith('alexnet') or args.arch.startswith('vgg'):
             model.features = torch.nn.DataParallel(model.features)
-            model.cuda()
+            if args.cuda:
+                model.cuda()
         else:
-            model = torch.nn.DataParallel(model).cuda()
+            model = torch.nn.DataParallel(model)
+            if args.cuda:
+                modle = model.cuda()
 
     # define loss function (criterion) and optimizer
-    criterion = nn.CrossEntropyLoss().cuda(args.gpu)
+    criterion = nn.CrossEntropyLoss()
+    if args.cuda:
+        criterion = criterion.cuda(args.gpu)
 
     optimizer = torch.optim.SGD(model.parameters(), args.lr,
                                 momentum=args.momentum,
@@ -178,7 +199,7 @@ def main_worker(gpu, ngpus_per_node, args):
     if args.resume:
         if os.path.isfile(args.resume):
             print("=> loading checkpoint '{}'".format(args.resume))
-            if args.gpu is None:
+            if args.gpu is None and args.cuda:
                 checkpoint = torch.load(args.resume)
             else:
                 # Map model to be loaded to specified single gpu.
@@ -186,7 +207,7 @@ def main_worker(gpu, ngpus_per_node, args):
                 checkpoint = torch.load(args.resume, map_location=loc)
             args.start_epoch = checkpoint['epoch']
             best_acc1 = checkpoint['best_acc1']
-            if args.gpu is not None:
+            if args.gpu is not None and args.cuda:
                 # best_acc1 may be from a checkpoint from a different GPU
                 best_acc1 = best_acc1.to(args.gpu)
             model.load_state_dict(checkpoint['state_dict'])
@@ -196,7 +217,8 @@ def main_worker(gpu, ngpus_per_node, args):
         else:
             print("=> no checkpoint found at '{}'".format(args.resume))
 
-    cudnn.benchmark = True
+    if args.cuda:
+        cudnn.benchmark = True
 
     # Data loading code
     traindir = os.path.join(args.data, 'train')
@@ -233,6 +255,8 @@ def main_worker(gpu, ngpus_per_node, args):
         num_workers=args.workers, pin_memory=True)
 
     if args.evaluate:
+        if args.mkldnn and not args.cuda:
+            print("using mkldnn model to do inference\n")
         validate(val_loader, model, criterion, args)
         return
 
@@ -281,9 +305,9 @@ def train(train_loader, model, criterion, optimizer, epoch, args):
         # measure data loading time
         data_time.update(time.time() - end)
 
-        if args.gpu is not None:
+        if args.gpu is not None and args.cuda:
             images = images.cuda(args.gpu, non_blocking=True)
-        if torch.cuda.is_available():
+        if args.cuda:
             target = target.cuda(args.gpu, non_blocking=True)
 
         # compute output
@@ -321,35 +345,104 @@ def validate(val_loader, model, criterion, args):
 
     # switch to evaluate mode
     model.eval()
+    if args.evaluate and args.mkldnn and not args.cuda:
+        model = mkldnn_utils.to_mkldnn(model)
 
-    with torch.no_grad():
-        end = time.time()
-        for i, (images, target) in enumerate(val_loader):
-            if args.gpu is not None:
-                images = images.cuda(args.gpu, non_blocking=True)
-            if torch.cuda.is_available():
-                target = target.cuda(args.gpu, non_blocking=True)
+    if args.dummy:
+        images = torch.randn(args.batch_size, 3, 224, 224)
+        target = torch.arange(1, args.batch_size + 1).long()
 
-            # compute output
-            output = model(images)
-            loss = criterion(output, target)
-
-            # measure accuracy and record loss
-            acc1, acc5 = accuracy(output, target, topk=(1, 5))
-            losses.update(loss.item(), images.size(0))
-            top1.update(acc1[0], images.size(0))
-            top5.update(acc5[0], images.size(0))
-
-            # measure elapsed time
-            batch_time.update(time.time() - end)
+        number_iter = len(val_loader)
+        with torch.no_grad():
             end = time.time()
 
-            if i % args.print_freq == 0:
-                progress.display(i)
+            for i in range(number_iter):
+                if args.gpu is not None and args.cuda:
+                    images = images.cuda(args.gpu, non_blocking=True)
+                if args.cuda:
+                    target = target.cuda(args.gpu, non_blocking=True)
 
-        # TODO: this should also be done with the ProgressMeter
-        print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
-              .format(top1=top1, top5=top5))
+                if args.mkldnn and not args.cuda:
+                    images = images.to_mkldnn()
+
+                # compute output
+                output = model(images)
+                if args.mkldnn and not args.cuda:
+                    output = output.to_dense()
+
+                loss = criterion(output, target)
+
+                # measure accuracy and record loss
+                acc1, acc5 = accuracy(output, target, topk=(1, 5))
+                if args.mkldnn and not args.cuda:
+                    images = images.to_dense()
+
+                losses.update(loss.item(), images.size(0))
+                top1.update(acc1[0], images.size(0))
+                top5.update(acc5[0], images.size(0))
+
+                # measure elapsed time
+                batch_time.update(time.time() - end)
+                end = time.time()
+
+                if i % args.print_freq == 0:
+                    progress.display(i)
+
+            if args.evaluate:
+                batch_size = val_loader.batch_size
+                latency = batch_time.avg / batch_size * 1000
+                perf = batch_size/batch_time.avg
+                print('inference latency %3.0f ms'%latency)
+                print('inference performance %3.0f fps'%perf)
+
+            # TODO: this should also be done with the ProgressMeter
+            print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+                .format(top1=top1, top5=top5))
+    else:
+        with torch.no_grad():
+            end = time.time()
+            for i, (images, target) in enumerate(val_loader):
+                if args.gpu is not None and args.cuda:
+                    images = images.cuda(args.gpu, non_blocking=True)
+                if args.cuda:
+                    target = target.cuda(args.gpu, non_blocking=True)
+
+                if args.mkldnn and not args.cuda:
+                    images = images.to_mkldnn()
+
+                # compute output
+                output = model(images)
+                if args.mkldnn and not args.cuda:
+                    output = output.to_dense()
+
+                loss = criterion(output, target)
+
+                # measure accuracy and record loss
+                acc1, acc5 = accuracy(output, target, topk=(1, 5))
+                if args.mkldnn and not args.cuda:
+                    images = images.to_dense()
+
+                losses.update(loss.item(), images.size(0))
+                top1.update(acc1[0], images.size(0))
+                top5.update(acc5[0], images.size(0))
+
+                # measure elapsed time
+                batch_time.update(time.time() - end)
+                end = time.time()
+
+                if i % args.print_freq == 0:
+                    progress.display(i)
+
+            if args.evaluate:
+                batch_size = val_loader.batch_size
+                latency = batch_time.avg / batch_size * 1000
+                perf = batch_size/batch_time.avg
+                print('inference latency %3.0f ms'%latency)
+                print('inference performance %3.0f fps'%perf)
+
+            # TODO: this should also be done with the ProgressMeter
+            print(' * Acc@1 {top1.avg:.3f} Acc@5 {top5.avg:.3f}'
+                .format(top1=top1, top5=top5))
 
     return top1.avg
 
